817007ae61afeebac7487cbf75df9a0188a1850a
ki4oir <ki4oir@users.noreply.github.com>
2016-01-08 00:52:23 -0500

    Create class C3.java

---
+++ class C3.java
@@ -0,0 +1,32 @@
+package c3;
+
+class C3 {
+	public static int n = 0;
+	public int m = 0;
+	public C3() {
+		n++; m++;
+	}
+	public void display() {
+		System.out.println(n + " " + m);
+	}
+	public static void main(String[] args) {
+		C3 c1 = new C3();
+		c1.display();
+		C3 c2 = new C3();
+		c1.display();
+		c2.display();
+		C3.n = 10;
+		c1.display();
+		c2.display();
+		c1.m = 11;
+		c2.m = 12;
+		c2.n = 14; //this static field C3.n should be accessed in a static way
+		c1.display();
+		c2.display();
+		C3.n = 16;
+		c1.display();
+		c2.display();
+		//note that the class name, C3, is used to access n and the object name is used to access m.
+	}
+
+}
